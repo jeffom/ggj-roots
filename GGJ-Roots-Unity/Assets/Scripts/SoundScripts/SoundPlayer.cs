59cb24b63d044a0dd380bd3854a58fffcb36b14f
RobinStarkgraff <rstarkgraff@gmx.net>
2023-02-04 17:48:47 +0100

    Adding music to the scenes

---
+++ GGJ-Roots-Unity/Assets/Scripts/SoundScripts/SoundPlayer.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundPlayer : MonoBehaviour
+{
+    [NonSerialized] public SoundPlayer Instance;
+
+    [SerializeField] private AudioClip bgmForScene;
+    [SerializeField] private AudioMixer audioMixer;
+
+    public static string SFX = "SFX";
+    public static string BGM = "BGM";
+
+    private void Awake()
+    {
+        if (Instance == null) return;
+
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        PlayBGM(bgmForScene, true);
+    }
+
+    public void SetLevel(float sliderValue)
+    {
+        audioMixer.SetFloat("Master_Volume", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void SetBGMLevel(float sliderValue)
+    {
+        audioMixer.SetFloat("BGM_Volume", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public AudioSource PlayBGM(
+        AudioClip clip,
+        bool loop = false,
+        float spatialBlend = 0,
+        float volume = 1
+    ) {
+        if (clip == null) return null;
+        return PlayOnGO(clip, BGM, Camera.main.gameObject, loop, spatialBlend, volume);
+    }
+
+    public AudioSource PlayOnGO(AudioClip clip, string audioMixerGroup, GameObject GO, bool loop = false,
+        float spatialBlend = 0, float volume = 1)
+    {
+        if (clip == null || GO == null) return null;
+
+        AudioSource source = GO.AddComponent<AudioSource>();
+        source.clip = clip;
+        source.loop = loop;
+        source.spatialBlend = spatialBlend;
+        source.volume = volume;
+
+        source.outputAudioMixerGroup = audioMixer.FindMatchingGroups(audioMixerGroup)[0];
+
+        source.Play();
+        if (!loop)
+        {
+            StartCoroutine(DestroySource(clip.length, source));
+        }
+
+        return source;
+    }
+
+    IEnumerator DestroySource(float time, AudioSource source)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(source);
+    }
+}
